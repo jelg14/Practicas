7c7263eebc7620437731128c8d3e863bbca940c0
Jose Lopez <jeduardo14lopez@gmail.com>
2021-03-30 18:17:07 -0600

    Adelanto hasta matrices

--- Practica 1/src/Main.java
+++ Practica 1/src/Main.java
@@ -1,20 +1,20 @@
   class Main{
 		/*
 		NOTAS IMPORTATES:
-			* Siempre se declara el tipo de variable a 
-				utilizar y su nombre.
+			*Siempre se declara el tipo de variable a usar y su nombre
 			*#1: sentencia IF
-			*#2: instruccion for
-			*#3: Despliegue numeros impares
-			*#4: Despliegue numeros Fibonacci
+			*#2: Despliegue numeros impares (Usando IF)
+			*#3: Despliegue numeros Fibonacci (Usando sentencia FOR)
+			*#4: sentencia SWITCH
+			*#5: arreglos
+			*#6: matrices
+			
 		*/
 	 public static void main(String[] args){
 		int var1 = 5151;
 		int var2=2;
 		
-	 System.out.println("Number: "+var1);
-		/* 
-			*#1*
+		// #1
 		if(var2==1){
 			var1+=1500;
 			System.out.println("suma = "+var1);
@@ -22,9 +22,9 @@
 			var1-=1000;
 			System.out.println("resta = "+var1);
 		}
-		*/
 		
-		//*#3
+		
+		//*#2
 		int x = 10;
 		for (int i=0; i<=x; i++){
 			if(i%2 != 0){
@@ -32,7 +32,8 @@
 			}
 		}
 		System.out.println(" ");
-		//#4
+		
+		//#3
 		int y=1;
 		int ant= 0;
 		int temp; /* var. temporal, donde se guarda valor de y antes
@@ -45,6 +46,45 @@
 			
 		}
 		
+		//#4
+			int v=1;
+			switch(v){
+				case 0:
+					System.out.println("opcion 0");
+				break;
+			
+				case 1:
+					System.out.println("opcion 1");
+				break;
+			
+				default:
+					System.out.println("Opcion por defecto");
+				break;
+			}
+		
+		//#5
+			int ar[] = new int[10];
+			ar[0] = 1;
+			ar[1] = 4;
+			ar[9] = 1000;
+			System.out.println("---------");
+			for(int i=0;i<ar.length;i++){
+				System.out.println(ar[i]);
+			}
+		//#6
+			int arg[][] = new int [2] [2];
+			arg[0][0] = 10;
+			arg[0][1] = 20;
+			arg[1][0] = 30;
+			arg[1][1] = 40;
+			
+			for(int i = 0; i<arg.length; i++){
+				for(int j = 0; j<arg.length; j++){
+					System.out.println(arg[i][j]);
+				}
+				
+			}
+		//#7
 	 }
  }
  
